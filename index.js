0febf501c7c8860982deb517e571db132523fbb2
James Hyatt <76410700+GreaterThanTechnologies@users.noreply.github.com>
2021-04-26 16:36:58 -0400

    commit tree 30a3f1cd2eeb5805e1d7501db947d0f121478d89

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Your code here
+function mapToNegativize(src) {
+  let result = []
+  for (let i = 0; i < src.length; i++) {
+    result.push(-1 * src[i])
+  }
+  return result
+}
+
+function mapToNoChange(src) {
+  let result = []
+  for (let i = 0; i < src.length; i++) {
+    result.push(src[i])
+  }
+  return result
+}
+
+function mapToDouble(src) {
+  let result = []
+  for (let i = 0; i < src.length; i++) {
+    result.push(src[i] * 2)
+  }
+  return result
+}
+
+function reduceToTotal(src, startingPoint=0) {
+  let result = startingPoint
+  for (let i = 0; i < src.length; i++) {
+    result = result + src[i]
+  }
+  return result
+}
+
+function mapToSquare(src) {
+  let result = []
+  for (let i = 0; i < src.length; i++) {
+    result.push(src[i] * src[i])
+  }
+  return result
+}
+
+function reduceToAllTrue(src) {
+  for (let i = 0; i < src.length; i++) {
+    if (!src[i]) return false
+  }
+  return true
+}
+
+function reduceToAnyTrue(src) {
+  for (let i = 0; i < src.length; i++) {
+    if (src[i]) return true
+  }
+  return false
+}
